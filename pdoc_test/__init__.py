5a48e3ab830193e230166fdcb5e47021717b3fe2
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-07-01 15:34:56 +0200

    expose callable

--- pdoc_test/__init__.py
+++ pdoc_test/__init__.py
@@ -5,7 +5,12 @@ __version__ = '0.0.1'
 
 # Add the public API by importing just the API symbols from ._internal.XXX
 
+from typing import Callable
+
 from ._internal.interface import MyInterface
 from ._internal.sample import MY_VAR, my_function
 
-__all__ = ['MyInterface', 'MY_VAR', 'my_function']
+MyType = Callable[[str], str]
+"""My function type"""
+
+__all__ = ['MyInterface', 'MY_VAR', 'my_function', 'MyType']
